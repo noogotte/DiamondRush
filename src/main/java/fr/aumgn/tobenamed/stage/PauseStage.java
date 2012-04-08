86cb85bade4d663290f2897d611cdc244cf6cd2b
aumgn <aumgn@free.fr>
2012-04-08 14:35:50 +0200

    Fix pause moves handling

--- src/main/java/fr/aumgn/tobenamed/stage/PauseStage.java
+++ src/main/java/fr/aumgn/tobenamed/stage/PauseStage.java
@@ -1,25 +1,63 @@
 package fr.aumgn.tobenamed.stage;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 import fr.aumgn.tobenamed.game.Game;
+import fr.aumgn.tobenamed.game.Team;
 import fr.aumgn.tobenamed.stage.listeners.PauseListener;
 
-public class PauseStage extends Stage{
+public class PauseStage extends Stage {
+
+    public static class PauseLocation {
+
+        private Location location;
+        private Material material;
+        private byte data;
+
+        public PauseLocation(Player player) {
+            Location loc = player.getLocation();
+            location = new Location(loc.getWorld(), loc.getBlockX() + 0.5, 
+                    loc.getBlockY(), loc.getBlockZ() + 0.5,
+                    loc.getYaw(), loc.getPitch());
+            Block block = location.getBlock().getRelative(BlockFace.DOWN);
+            material = block.getType();
+            data = block.getData();
+            block.setType(Material.GLASS);
+            teleportTo(player);
+        }
+
+        public void teleportTo(Player player) {
+            player.teleport(location);
+        }
+
+        public void restore() {
+            Block block = location.getBlock().getRelative(BlockFace.DOWN);
+            block.setTypeIdAndData(material.getId(), data, true);
+        }
+    }
 
     private Game game;
     private Stage oldStage;
     private PauseListener listener;
     private long time;
+    private Map<Player, PauseLocation> locations;
 
     public PauseStage(Game game, Stage oldStage){
         this.game = game;
         this.oldStage = oldStage;
-        this.listener = new PauseListener(game);
+        this.listener = new PauseListener(this);
+        this.locations = new HashMap<Player, PauseStage.PauseLocation>();
     }
 
     @Override
@@ -37,12 +75,24 @@ public class PauseStage extends Stage{
         this.time = game.getWorld().getTime();
         game.sendMessage(ChatColor.RED +
                 "TBN est en pause, vous ne pouvez rien faire !");
+        for (Team team : game.getTeams()) {
+            for (Player player : team.getPlayers()) {
+                locations.put(player, new PauseLocation(player));
+            }
+        }
     }
 
     @Override
     public void stop() {
         game.getWorld().setTime(time);
         game.sendMessage(ChatColor.GREEN + "La partie reprend !");
+        for (PauseLocation location : locations.values()) {
+            location.restore();
+        }
+    }
+
+    public PauseLocation getPauseLocation(Player player) {
+        return locations.get(player);
     }
 
     public Stage getOldStage() {
--- src/main/java/fr/aumgn/tobenamed/stage/listeners/PauseListener.java
+++ src/main/java/fr/aumgn/tobenamed/stage/listeners/PauseListener.java
@@ -1,8 +1,5 @@
 package fr.aumgn.tobenamed.stage.listeners;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,33 +10,29 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import fr.aumgn.tobenamed.game.Game;
-import fr.aumgn.tobenamed.game.Team;
+import fr.aumgn.tobenamed.stage.PauseStage;
 
 public class PauseListener implements Listener {
 
+    private PauseStage stage;
     private Game game;
-    private Map<Player, Location> positions;
 
-    public PauseListener(Game game) {
-        this.game = game;
-        this.positions = new HashMap<Player, Location>();
-        for (Team team : game.getTeams()) {
-            for (Player player : team.getPlayers()) {
-                positions.put(player, player.getLocation());
-            }
-        }
+    public PauseListener(PauseStage stage) {
+        this.stage = stage;
+        this.game = stage.getGame();
     }
 
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
         Location from = event.getFrom();
-        Location to = event.getFrom();
+        Location to = event.getTo();
         if (from.getBlockX() != to.getBlockX()
                 || from.getBlockY() != to.getBlockY()
                 || from.getBlockZ() != to.getBlockZ()) {
             if (game.contains(player)) {
-                event.setTo(positions.get(player));
+                stage.getPauseLocation(player).
+                    teleportTo(player);
             }
         }
     }
