492e4a7f4cd33a2156287bad32341fd1f27e1daf
aumgn <aumgn@free.fr>
2012-04-08 14:51:12 +0200

    Add missing event in for pause stage

--- src/main/java/fr/aumgn/tobenamed/stage/listeners/PauseListener.java
+++ src/main/java/fr/aumgn/tobenamed/stage/listeners/PauseListener.java
@@ -1,9 +1,14 @@
 package fr.aumgn.tobenamed.stage.listeners;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
+import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -57,6 +62,38 @@ public class PauseListener implements Listener {
     public void onInteractEntity(PlayerInteractEntityEvent event) {
         Player player = event.getPlayer();
         if (game.contains(player)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof Player && game.contains((Player) entity)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onRegainHealth(EntityRegainHealthEvent event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof Player && game.contains((Player) entity)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onFoodLevelChange(FoodLevelChangeEvent event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof Player && game.contains((Player) entity)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onTarget(EntityTargetEvent event) {
+        Entity entity = event.getTarget();
+        if (entity instanceof Player && game.contains((Player) entity)) {
             event.setCancelled(true);
         }
     }
