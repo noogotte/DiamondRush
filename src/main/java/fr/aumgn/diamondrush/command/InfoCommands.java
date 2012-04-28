ba4bd3fb3b12f5e44a491a42bb3b7ad3d01fdb96
aumgn <aumgn@free.fr>
2012-04-28 11:11:35 +0200

    Add spectators commands

--- src/main/java/fr/aumgn/diamondrush/command/InfoCommands.java
+++ src/main/java/fr/aumgn/diamondrush/command/InfoCommands.java
@@ -1,6 +1,7 @@
 package fr.aumgn.diamondrush.command;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import fr.aumgn.bukkitutils.command.Command;
 import fr.aumgn.bukkitutils.command.CommandArgs;
@@ -23,5 +24,17 @@ public class InfoCommands extends DiamondRushCommands {
         for (String message : view) {
             sender.sendMessage(message);
         }
+    }
+
+    @Command(name = "spectators")
+    public void spectators(CommandSender sender, CommandArgs args) {
+        StringBuilder builder = new StringBuilder();
+        for (Player spectator : dr.getGame().getSpectators()) {
+            builder.append(spectator.getDisplayName());
+            builder.append(" ");
+        }
+
+        sender.sendMessage("Spectateurs :");
+        sender.sendMessage(builder.toString());
     }
 }
